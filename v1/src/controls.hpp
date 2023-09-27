cdc1473f7f337ff750947622e319e0aacde11387
8pxl <56054380+8pxl@users.noreply.github.com>
2023-09-26 21:52:00 -0700

    basic controls

--- v1/src/controls.hpp
+++ v1/src/controls.hpp
@@ -3,23 +3,24 @@
 // #include "util.hpp"
 
 using namespace lib;
-void driver()
-{
-        // std::vector<bool> state = robot::controller.getAll(ALLBUTTONS);
-        // robot::chassMtrs.spinDiffy(robot::controller.drive(-1, lib::controller::arcade));
+
+bool toggle = false;
+void driver() {
+        std::vector<bool> state = robot::controller.getAll(ALLBUTTONS);
+        robot::chassMtrs.spinDiffy(robot::controller.drive(1, lib::controller::arcade));
         
-        // if(state[NR1]) robot::tsukasa.setState(false);
-        // if(state[R1]) robot::intake.spin(127);
-        // else if(state[R2]) robot::intake.spin(-127);
-        // else robot::intake.stop('c');
+        if(state[R1]) robot::intake.spin(127);
+        else if(state[R2]) robot::intake.spin(-127);
+        else robot::intake.stop('c');
 
-        // if(state[L1]) robot::puncher.spin(127);
-        // else if(!puncherToggled) robot::puncher.stop('c');
+        if(state[L1]) robot::cata.spin(127);
+        else if(!toggle) robot::cata.stop('c');
 
-        // if(state[NL2]) robot::angler.toggle();
-        // if(state[NUP]) robot::tsukasa.toggle();
+        if(state[NUP]) robot::tsukasa.toggle();
 
-        // if(puncherToggled) robot::puncher.spin(127);
-        // if(state[NA]) puncherToggled = !puncherToggled;
-        // pros::delay(20);
+        if(toggle) robot::cata.spin(127);
+        if(state[NA]) toggle = !toggle;
+
+        if(state[NL2]) robot::wings.toggle();
+        pros::delay(20);
 }
