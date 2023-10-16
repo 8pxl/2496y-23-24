ad25da6bce717f68e7c3b219ecc43084641658c7
8pxl <56054380+8pxl@users.noreply.github.com>
2023-10-16 14:11:21 -0700

    Update controls.hpp

--- v1/src/controls.hpp
+++ v1/src/controls.hpp
@@ -11,19 +11,18 @@ void driver() {
 
         if(state[L2]) {
             if (state[NL1]) scraper.toggle();
-            // if (state[NR1]) robot::wings.toggle();
             if (state[R1]) robot::blocker.spin(127);
             else if (state[R2]) robot::blocker.spin(-127);
             else robot::blocker.stop('h');
         }
         
         else {
-            if(state[NL1]) cata::fire();
-            if(state[NB]) cata::halfway();
-            if(state[NA]) cata::toggle();
+            if (state[NL1]) cata::fire();
+            if (state[NB]) cata::halfway();
+            if (state[NA]) cata::toggle();
 
-            if(state[R1]) robot::intake.spin(127);
-            else if(state[R2]) robot::intake.spin(-127);
+            if (state[R1]) robot::intake.spin(127);
+            else if (state[R2]) robot::intake.spin(-127);
             else robot::intake.stop('c');
 
             if (state[X]) robot::blocker.stop('c');
@@ -36,25 +35,3 @@ void driver() {
         cata::cataControl();
         pros::delay(20);
 }
-
-
-// bool toggle = false;
-// void driver() {
-//         std::vector<bool> state = robot::controller.getAll(ALLBUTTONS);
-//         robot::chassMtrs.spinDiffy(robot::controller.drive(1, lib::controller::arcade));
-        
-//         if(state[R1]) robot::intake.spin(127);
-//         else if(state[R2]) robot::intake.spin(-127);
-//         else robot::intake.stop('c');
-
-//         if(state[L1]) robot::cata.spin(127);
-//         else if(!toggle) robot::cata.stop('c');
-
-//         if(state[NUP]) robot::tsukasa.toggle();
-
-//         if(toggle) robot::cata.spin(127);
-//         if(state[NA]) toggle = !toggle;
-
-//         if(state[NL2]) robot::wings.toggle();
-//         pros::delay(20);
-// }
