4c63bbb741707c19da69a8f7fce7800542a31c39
8pxl <56054380+8pxl@users.noreply.github.com>
2023-12-20 12:15:22 -0800

    scheduler 2

---
+++ v1/src/keejLib/util/scheduler.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "main.h"
+#include "aayush.h"
+#include "../include/keejLib/lib.h"
+using namespace lib;
+using namespace pros;
+namespace lib {
+    
+    void lib::scheduler::run() {
+        auto it = upper_bound(tasks.begin(), tasks.end(), pros::millis(), [](const uint32_t a, const task &b) {
+            return (b.start < a);
+        });
+
+        if (it != tasks.end()) {
+            uint32_t time = pros::millis();
+            for(auto q = tasks.begin(); q != it; q++) {
+                task t = *q;
+                t.task();
+            }
+        }
+    }
+
+    void lib::scheduler::add(fptr item, int start) {
+        task t = {item, start + (int) pros::millis()};
+        tasks.insert(t);
+    }
+
+    void lib::scheduler::init() {
+        mainTask = new pros::Task {[=] {
+            while (true) {
+                run();
+                pros::delay(10);
+            }
+        }};
+    }
+}
